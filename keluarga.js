2f7c222079c311d1c7590bc9a677d051b97ba954
syafnilyya <syafniaulyya81@gmail.com>
2025-06-06 20:02:01 +0700

    Add files via upload

---
+++ keluarga.js
@@ -0,0 +1,12 @@
+const keluarga = [
+    ["Budi Sudarsono", "Ayah"], // Ayah
+    ["Ayu Sinta Putri", "Ibu"], // Ibu
+    ["Joko", "Anak"], // Anak 1
+    ["Dina", "Anak"], // Anak 2
+    ["April", "Anak"], // Anak 3
+    ["Alif", "Anak"], // Anak 4
+    ["Doni", "Anak"], // Anak 5
+];
+
+// Menampilkan output yang diminta
+console.log(keluarga[4][0] + " adalah anak dari " + keluarga [1][0]);
